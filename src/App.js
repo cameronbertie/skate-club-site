867d77abc232e9367539e05516cfc353ff016c8f
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2025-11-13 19:19:36 +0000

    Deploy old version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Skate Together! <span>🛹</span></h1>
+
+      <div class="community">
+        Join the community for new and beginner adult skateboarders<br/><a href="https://www.instagram.com/cameronbertie" target="_blank" >DM @cameronbertie on Instagram to learn more</a>
+      </div>
+      
+      <div class="attendee-count">
+        First skate meet on Sunday 30th November 2025
+      </div>
+
+      <div class="card">
+
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/BAYSIXTY6_Skate_Park_2024-06-05.jpg/960px-BAYSIXTY6_Skate_Park_2024-06-05.jpg?20240615160544" alt="Peak District"/>
+
+        <div class="card-footer">
+          📍BAYSIXTY6 SKATE PARK<br></br>📅SUNDAY 30TH NOVEMBER 2025<br></br>⌚10:00AM
+        </div>
+      
+      </div>
+
+      <div class="info">
+        <br></br>
+        <h2>🛹BAYSIXTY6 SKATE MEET</h2>
+        <h3>SUNDAY 30TH NOVEMBER 2025<br></br>10:00AM - 12:00PM</h3>
+        <h3>BAYSIXTY6 SKATE PARK
+          <br></br>Bay 66 Acklam Road<br></br>North Kensington<br></br>London<br></br>W10 5YU</h3><br></br>
+          <p>Get ready for the first ever Improver Skate Meet — a morning to skate, connect, and push your limits with other skaters that are in the same boat as you.</p>
+          <p>We'll be heading to BAYSIXTY6 Skate Park and we will be attending the beginners session so that it's a safer environment for everyone in our group and ideal for those still getting comfortable on their board.</p>
+          <p>The skatepark will be shared with other skateboarders outside our group - but don't worry, there's plenty of space for everyone to ride and enjoy the session.</p><br></br>
+          <h3>⚠️<u>REMEMBER YOUR HELMETS!</u></h3>
+          <p>Everyone is required to wear a helmet to skate at this park. If you don’t have one, no problem — helmets are available to rent at the door.</p>
+          <br></br>
+          <h3>📝<u>REMEMBER TO REGISTER AND READ THE WAIVER</u></h3>
+          <p>Also, everyone is required to register on the BAYSIXTY6 website and complete their waiver form online before entering the skatepark. If you haven’t done so already, you can register using the link below.<br></br><a href="https://www.baysixty6.com/new-membership-registration/" target="_blank" >https://www.baysixty6.com/new-membership-registration/</a></p>
+          <br></br>
+          <h3>🛹<u>BRING YOUR OWN BOARD</u></h3>
+          <p>And of course, don’t forget your skateboard! If you don’t have one, you can rent a board at the door — just keep in mind that rentals are first come, first served, and availability isn’t guaranteed on the day.</p>
+          <br></br>
+          <h3>😀<u>AND FINALLY...</u></h3>
+          <p>BaySixty6 Skate Park charges an entry fee to use the park, but as this is our first ever skate meet, your entry will be completely covered! If you’d like to join us, just message the meetup organiser @cameronbertie
+          on Instagram for more details and please don't hesitate to ask any questions.</p>
+          <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+      </div>
+
     </div>
   );
 }
